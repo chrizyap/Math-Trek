4fd4da61daf8e953aadfc52a5a45c42d49aa22fb
Christopher Adeogun-Phillips <christopheraphillips@christophers-macbook-pro.local>
2019-04-12 18:18:13 +0100

    Initial Commit

---
+++ Math Trek/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Math Trek
+//
+//  Created by Christopher Adeogun-Phillips on 12/04/2019.
+//  Copyright © 2019 Christopher Adeogun-Phillips. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Math TrekTests/Math_TrekTests.swift
@@ -0,0 +1,34 @@
+//
+//  Math_TrekTests.swift
+//  Math TrekTests
+//
+//  Created by Christopher Adeogun-Phillips on 12/04/2019.
+//  Copyright © 2019 Christopher Adeogun-Phillips. All rights reserved.
+//
+
+import XCTest
+@testable import Math_Trek
+
+class Math_TrekTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
